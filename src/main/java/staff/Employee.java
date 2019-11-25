d8f239dae647f6b70bbcd93fbd420f60a8933584
Mark Payne <mjpayne8@gmail.com>
2019-11-25 15:16:24 +0000

    employee class created

---
+++ src/main/java/staff/Employee.java
@@ -0,0 +1,35 @@
+package staff;
+
+public abstract class Employee {
+
+    private String name;
+    private String niNumber;
+    private double salary;
+
+    public Employee(String name, String niNumber, Double salary){
+        this.name = name;
+        this.niNumber = niNumber;
+        this.salary = salary;
+    }
+
+    public String getName(){
+        return this.name;
+    };
+
+    public String getNiNumber(){
+        return this.niNumber;
+    }
+
+    public double salary(){
+        return this.salary;
+    }
+
+    public void raiseSalary(double raise){
+        this.salary += raise;
+    }
+
+    public double payBonus(){
+        return 0.01*this.salary;
+    }
+
+}
