285041635aebeb90908c49870216f501e6d05912
Mark Payne <mjpayne8@gmail.com>
2019-11-25 15:47:08 +0000

    developer class created and tested

--- src/test/java/DeveloperTest.java
+++ src/test/java/DeveloperTest.java
@@ -1,2 +1,41 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.techStaff.Developer;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class DeveloperTest {
+
+    private Developer developer;
+
+    @Before
+    public void before(){
+        developer = new Developer("James", "S174637829D",21000);
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("James", developer.getName());
+    }
+
+    @Test
+    public void canGetNiNumber(){
+        assertEquals("S174637829D", developer.getNiNumber());
+    }
+
+    @Test
+    public void canGetSalary(){
+        assertEquals(21000, developer.getSalary(), 0.001);
+    }
+
+    @Test
+    public void canRaiseSalary(){
+        developer.raiseSalary(1000);
+        assertEquals(22000, developer.getSalary(),0.001);
+    }
+
+    @Test
+    public void canPayBonus(){
+        assertEquals(210, developer.payBonus(),0.001);
+    }
 }
