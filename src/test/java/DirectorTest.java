283e0c983dcb864f9ce7b902c6d1ce5b6c5fee24
Mark Payne <mjpayne8@gmail.com>
2019-11-25 15:55:35 +0000

    director class created and tested

---
+++ src/test/java/DirectorTest.java
@@ -0,0 +1,47 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.management.Director;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class DirectorTest {
+
+    private Director director;
+
+    @Before
+    public void before(){
+        director = new Director("Tim","J74839382D",100000,"Insurance", 1000000);
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Tim", director.getName());
+    }
+
+    @Test
+    public void canGetNiNumber(){
+        assertEquals("J74839382D", director.getNiNumber());
+    }
+
+    @Test
+    public void canGetSalary(){
+        assertEquals(100000, director.getSalary(), 0.001);
+    }
+
+    @Test
+    public void canGetDeptname(){
+        assertEquals("Insurance", director.getDeptName());
+    }
+
+    @Test
+    public void canRaiseSalary(){
+        director.raiseSalary(1000);
+        assertEquals(101000, director.getSalary(),0.001);
+    }
+
+    @Test
+    public void canPayBonus(){
+        assertEquals(1000, director.payBonus(),0.001);
+    }
+
+}
